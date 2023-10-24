c480a2bbde901b884beed31522e7f8e352e14761
jaketothepast <windlejacob12@gmail.com>
2023-10-23 21:35:32 -0400

    Depth-first search on the DOM, identify elements - to use in the scrape

--- src/main.lisp
+++ src/main.lisp
@@ -1,8 +1,7 @@
-(ql:quickload "dexador")
-
 (defpackage :test-project
   (:import-from :dexador)
-  (:use :cl :vecto :plotlib :lquery :trivia))
+  (:use
+   :cl :vecto :plotlib :lquery :trivia :plump))
 (in-package :test-project)
 
 (defvar *all-children-query* "html > *")
@@ -10,16 +9,22 @@
 (defun main ()
   (plotlib:test-plotlib "file.png"))
 
-(defvar *doc* ($ (initialize (dex:get "https://wjhl.com"))))
-
 (defun get-all-children (root-node)
   ($ root-node *all-children-query*))
 
+(defvar *doc* ($ (initialize (dex:get "https://wjhl.com"))))
 (defvar *all-children* (get-all-children *doc*))
 
-(defun walk-dom-tree (root)
-  "Depth-first search of the DOM, using Trivia for matching.")
+(defun walk-dom-tree (root reporter)
+  "Depth-first search of the DOM, using Trivia for matching."
+  (match root
+    ((vector*)
+     (loop for i across root do (walk-dom-tree i reporter)))
+    ((type element)
+     (funcall reporter root)
+     ;; Including the amount of children
+     (walk-dom-tree (plump:children root) reporter))))
 
-(walk-dom-tree *all-children*)
-
+;; Show the dom tree walked with text nodes.
+(walk-dom-tree *all-children* (lambda (node) (princ (plump:children node))))
 (main)
